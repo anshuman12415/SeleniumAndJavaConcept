dbc35be7387aed8dd9827f54e06e5ad562557ff3
anshuman <anshuman12415@gmail.com>
2020-12-29 22:30:05 +0530

    data types committed

--- src/javaProg/DataTypes.java
+++ src/javaProg/DataTypes.java
@@ -1,5 +1,22 @@
 package javaProg;
 
 public class DataTypes {
+	
+	public static void main(String[] args) {
+		int a=10;
+		double b=11.12;
+		char c='A';
+		String s="anshuman";
+		boolean k=true;
+		
+		
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(s);
+		System.out.println(k);
+		
+	}
 
 }
